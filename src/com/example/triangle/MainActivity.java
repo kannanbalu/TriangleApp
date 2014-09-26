99d3dd1c03409a7783a03c5052c22534cf26fa9b
Kannan <kannanbalu@yahoo.com>
2014-09-27 00:54:25 +0530

    Enhanced minor functionalities related to navigation
    1. Pressing Enter/Return on the input text field should trigger the
    validation operation
    2. Clicking Done button on the device keyboard should trigger the
    validation operation
    3. Copy to clipboard operation implemented
    4.  Enabled floating point #s as input in the text field

--- src/com/example/triangle/MainActivity.java
+++ src/com/example/triangle/MainActivity.java
@@ -4,6 +4,7 @@ import com.example.triangle.R;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ClipData;
 import android.content.DialogInterface;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -14,9 +15,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.ClipboardManager;
 
 /**
  * Class for creating a user interface for Triangle App , to be run on an AVD emulator or any Android device 
@@ -25,7 +29,7 @@ import android.widget.TextView;
  */
 public class MainActivity extends Activity {
 
-	public final char [] valid_chars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', ' '};
+	public final char [] valid_chars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', ' ','.'};
 	
 	public final String SCALENE_TRIANGLE = "Scalene Triangle";
 	public final String ISOSCELES_TRIANGLE = "Isosceles Triangle";
@@ -39,6 +43,7 @@ public class MainActivity extends Activity {
 	String triangleType = "";
 	View view = null;
 	Canvas canvas = null;
+	boolean bFloat = false;
 	
 	/**
 	 * Method loading all the required UI resources from activity_main.xml 
@@ -49,6 +54,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView tView = (TextView)findViewById(R.id.textView2);
+        tView.setTextColor(Color.BLUE);
         tView.setText("Result");
         final EditText input_value = (EditText)findViewById(R.id.editText1);
         
@@ -57,10 +63,16 @@ public class MainActivity extends Activity {
         validateButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
            
+            	if (input_value.getText().toString().length() == 0) {
+             		tView.setText(errMsg);
+            		exitApp();
+            	}
             	if (parseInput(input_value.getText().toString())) {
             		tView.setText(triangleType);
+            		copyToClipboard(triangleType);
             	} else {
             		tView.setText(errMsg);
+            		copyToClipboard(errMsg);
             	}
             }
         });
@@ -71,10 +83,35 @@ public class MainActivity extends Activity {
             	input_value.setText("");
             }
         });
-        /*view = (View) findViewById(R.id.vMain);
-        canvas = new Canvas();
-        draw(canvas);
-        view.draw(canvas);*/
+        
+        input_value.setOnEditorActionListener(new TextView.OnEditorActionListener() {        	
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            	if (input_value.getText().toString().length() != 0) {
+            		clearButton.setEnabled(true);
+            		validateButton.setEnabled(true);
+            	} else {
+            		clearButton.setEnabled(false);
+            		validateButton.setEnabled(false);
+            	}
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                	if (input_value.getText().toString().length() == 0) {
+                 		tView.setText(errMsg);
+                		exitApp();
+                 	    return true;
+                	}
+                	if (parseInput(input_value.getText().toString())) {
+                		tView.setText(triangleType);
+                		copyToClipboard(triangleType);
+                	} else {
+                		tView.setText(errMsg);
+                		copyToClipboard(errMsg);
+                	}
+                } else {
+                	parseInput(input_value.getText().toString());
+                }
+                return true;
+            }
+        });
     }
 
     public void draw(Canvas c) {
@@ -86,7 +123,6 @@ public class MainActivity extends Activity {
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.WHITE);
         c.drawPaint(paint);
-        
         paint.setColor(Color.parseColor("#CD5C5C"));
         c.drawCircle(x / 2, y / 2, radius, paint);
     }
@@ -99,6 +135,7 @@ public class MainActivity extends Activity {
     public boolean parseInput(String str) {
         String value = str.trim();
         errMsg = "";
+        bFloat = false;
         int commas = 0;
         int spaces = 0;
         for (int i=0; i<value.length(); i++) {
@@ -122,19 +159,14 @@ public class MainActivity extends Activity {
             	spaces++;
             }
         }
-        if (value.equals("0")) {
-        	errMsg = "Application will exit!";
-        	AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        	dialog.setTitle("Triangle App");
-        	dialog.setMessage("Thanks for using TriangleApp. Goodbye!");
-        	dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					System.exit(0);
-				}
-			});
-        	dialog.create().show();
-        	return false;
+        try {
+        	 Double val = Double.valueOf(value);
+        	 if (val == 0) {
+        		 exitApp();
+        		 return false;
+        	 }
+        } catch (Exception e) {
+        	//Don't do anything here
         }
 
         if (commas == 0 && spaces == 0) {
@@ -172,21 +204,42 @@ public class MainActivity extends Activity {
         String side_2 = str.substring(index+1, str.indexOf(DELIM, index+1)).trim();
         String side_3 = str.substring(str.indexOf(DELIM, index+1)+1, str.length()).trim();
         if (side_3.isEmpty()) {
-            errMsg = "length of side 3 cannot be empty";
+            errMsg = "invalid input (need more input). length of side 3 cannot be empty";
             return false;
         }
         errMsg = side_1 + "/" + side_2 + "/" + side_3;
-        int side1 = Integer.valueOf(side_1);
+        if (side_1.indexOf(".") != -1 || side_2.indexOf(".") != -1 || side_3.indexOf(".") != -1) {
+        	bFloat = true;
+        }
+        Double side1;
+        try {
+        	side1 = Double.valueOf(side_1);
+        } catch (Exception e) {
+        	errMsg = "[invalid input].  Please specify valid value for side1";
+        	return false;
+        }
         if (side1 < 1 || side1 > 100) {
         	errMsg = "Length of side one should be between 1 to 100";
         	return false;
         }
-        int side2 = Integer.valueOf(side_2);
+        Double side2;
+        try {
+        	side2 = Double.valueOf(side_2);
+        } catch (Exception e) {
+        	errMsg = "[invalid input].  Please specify valid value for side2";
+        	return false;
+        }
         if (side2 < 1 || side2 > 100) {
         	errMsg = "Length of side two should be between 1 to 100";
         	return false;
         }
-        int side3 = Integer.valueOf(side_3);
+        Double side3;
+        try {
+        	side3 = Double.valueOf(side_3);
+        } catch (Exception e) {
+        	errMsg = "[invalid input].  Please specify valid value for side3";
+        	return false;
+        }
         if (side3 < 1 || side3 > 100) {
         	errMsg = "Length of side three should be between 1 to 100";
         	return false;
@@ -197,6 +250,20 @@ public class MainActivity extends Activity {
         determineTriangleType(side1, side2, side3);
         return true ;
     }
+
+    public void exitApp() {
+    	errMsg = "Application will exit!";
+    	AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+    	dialog.setTitle("Triangle App");
+    	dialog.setMessage("Thanks for using TriangleApp. Goodbye!");
+    	dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				System.exit(0);
+			}
+		});
+    	dialog.create().show();
+    }
     
    /**
     * Method to determine if a valid triangle can be formed with length of 3 sides given
@@ -205,7 +272,7 @@ public class MainActivity extends Activity {
     * @param side3 length of third side
     * @return true if the triangle is a valid one, false otherwise
     */
-   public boolean isValidTriangle(int side1, int side2, int side3) {
+   public boolean isValidTriangle(double side1, double side2, double side3) {
        if (side1 + side2 < side3) {
         	errMsg = "Invalid triangle: sum of side1 and side2 is less than side3: ( " + side1 + " + " + side2 + " ) " + " < " + side3;
          	return false;
@@ -226,14 +293,26 @@ public class MainActivity extends Activity {
     * @param length of third side
     * @return a string containing the triangle type (Equilateral or Isosceles or Scalene)
     */
-   public String determineTriangleType(int a, int b, int c) {
+   public String determineTriangleType(double a, double b, double c) {
 	   String type = "";
 	   if (a == b && b == c) {
-		   type = EQUILATERAL_TRIANGLE;
+		   if (bFloat) {
+			   type = "[" + a + "," + b + "," + c + "] = " + EQUILATERAL_TRIANGLE;   
+		   } else {
+			   type = "[" + (int)a + "," + (int)b + "," + (int)c + "] = " + EQUILATERAL_TRIANGLE;
+		   }
 	   } else if (a == b || b == c || a == c) {
-		   type = ISOSCELES_TRIANGLE;
+		   if (bFloat) {
+			   type = "[" + a + "," + b + "," + c + "] = " + ISOSCELES_TRIANGLE;;   
+		   } else {
+			   type = "[" + (int)a + "," + (int)b + "," + (int)c + "] = " + ISOSCELES_TRIANGLE;;
+		   }
 	   } else {
-	       type = SCALENE_TRIANGLE;
+		   if (bFloat) {
+			   type = "[" + a + "," + b + "," + c + "] = " + SCALENE_TRIANGLE;   
+		   } else {
+			   type = "[" + (int)a + "," + (int)b + "," + (int)c + "] = " + SCALENE_TRIANGLE;
+		   }
 	   }
 	   triangleType = type;
 	   return type;
@@ -270,6 +349,15 @@ public class MainActivity extends Activity {
 	   g.drawLine(side1, 0, pt.x, pt.y, paint);
    }
     
+   /**
+    * Method to copy a given text to the clipboard
+    * @param copyString Text/String to be copied to the clipboard
+    */
+   public void copyToClipboard(String copyString) {
+       ClipboardManager mgr  = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+       ClipData clip = ClipData.newPlainText("TriangleApp", copyString);
+       mgr.setPrimaryClip(clip);
+   }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
